326fadb9d69027c5de5fbfe7c79af3e5fbec7427
riichardgabriel <141831980+riichardgabriel@users.noreply.github.com>
2024-02-19 11:54:18 -0300

    Add files via upload

---
+++ jogodaforca/jogodaforca/FormJogo.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace jogodaforca
+{
+    partial class FormJogo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbPalavraSecreta = new System.Windows.Forms.Label();
+            this.btnVerificarLetra = new System.Windows.Forms.Button();
+            this.txbTentativa = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbPalavraSecreta
+            // 
+            this.lbPalavraSecreta.AutoSize = true;
+            this.lbPalavraSecreta.Location = new System.Drawing.Point(58, 134);
+            this.lbPalavraSecreta.Name = "lbPalavraSecreta";
+            this.lbPalavraSecreta.Size = new System.Drawing.Size(81, 13);
+            this.lbPalavraSecreta.TabIndex = 0;
+            this.lbPalavraSecreta.Text = "Palavra secreta";
+            // 
+            // btnVerificarLetra
+            // 
+            this.btnVerificarLetra.Location = new System.Drawing.Point(314, 234);
+            this.btnVerificarLetra.Name = "btnVerificarLetra";
+            this.btnVerificarLetra.Size = new System.Drawing.Size(130, 23);
+            this.btnVerificarLetra.TabIndex = 1;
+            this.btnVerificarLetra.Text = "Verificar Letra";
+            this.btnVerificarLetra.UseVisualStyleBackColor = true;
+            this.btnVerificarLetra.Click += new System.EventHandler(this.btnVerificarLetra_Click);
+            // 
+            // txbTentativa
+            // 
+            this.txbTentativa.Location = new System.Drawing.Point(61, 234);
+            this.txbTentativa.Name = "txbTentativa";
+            this.txbTentativa.Size = new System.Drawing.Size(216, 20);
+            this.txbTentativa.TabIndex = 2;
+            // 
+            // FormJogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbTentativa);
+            this.Controls.Add(this.btnVerificarLetra);
+            this.Controls.Add(this.lbPalavraSecreta);
+            this.Name = "FormJogo";
+            this.Text = "FormJogo";
+            this.Load += new System.EventHandler(this.FormJogo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbPalavraSecreta;
+        private System.Windows.Forms.Button btnVerificarLetra;
+        private System.Windows.Forms.TextBox txbTentativa;
+    }
+}
---
+++ jogodaforca/jogodaforca/FormJogo.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jogodaforca
+{
+    public partial class FormJogo : Form
+    {
+        public FormJogo()
+        {
+            InitializeComponent();
+        }
+
+        string letrasTentadas = "";
+        string palavra_secreta = "";
+
+        private void FormJogo_Load(object sender, EventArgs e)
+        {
+            palavra_secreta = SortearPalavra();
+            lbPalavraSecreta.Text = palavra_secreta;
+        }
+
+        private string SortearPalavra()
+        {
+            //Lista de palavras
+            List<string> palavras = new List<string> { "csharp", "python", "ruby", "php", "javascript" };
+
+            Random rnd = new Random();
+            int indiceSorteado = rnd.Next(0, palavras.Count);
+
+            //Recuperar a palavra correspondente ao indice sorteado
+            string palavraSorteada = palavras[indiceSorteado];
+
+            return palavraSorteada;
+        }
+
+        private void btnVerificarLetra_Click(object sender, EventArgs e)
+        {
+            string letra = txbTentativa.Text.ToLower();
+            
+            bool letraRepetida = false; 
+            for (int i = 0; i < letrasTentadas.Length; i++)
+            {
+                letraRepetida = false;
+                if (letrasTentadas[i].ToString() == letra)
+                {
+                    MessageBox.Show("Essa letra já foi jogada, tente novamente !!! ");
+                    letrarepetida = true;
+                }
+            }
+
+            int verificarseganhou = 0; 
+
+            if (!letraRepetida)
+            {
+                letrasTentadas += letra;
+                char[] letrasDaPalavra = SepararLetraPalavra(palavra_secreta);
+                string palavra_exibicao = verificarPalavra(letrasDaPalavra, letrasTentadas.ToCharArray());
+
+                lbPalavraSecreta.Text = palavra_exibicao;
+
+                verificarSeGanhou = 0;
+                for (int i = 0; i < palavra_exibicao.Length; i++)
+                {
+                    if (palavra_exibicao[i] == '_')
+                    {
+                        verificarSeGanhou++;
+                    }
+                }
+            }
+
+            if (verificarSeGanhou == 0)
+            {
+                MessageBox.Show("Você ganhou !!!");
+                this.Close();
+            }
+
+            txbTentativa.Text = "";
+        }
+        private char[] SepararLetraPalavra(string palavra)
+        {
+            //criando um array do tamanho da palavra que foi sorteada 
+            char[] letraPalavra = new char[palavra.Length];
+
+            letrasPalavra = palavra.ToCharArray();
+
+            return letrasPalavra;
+        }
+        
+        private string VerificarPalavra(char[] palavra, char[] letraTentadas)
+        {
+            string palavra_escondida = ""; 
+
+            //laço de repetição para 'esconder' a palavra
+            for (int i = 0; i <palavra.Length; i++)
+            {
+                bool letraAcertada = false;
+                for (int j = 0; j < letraTentadas.Length; j++)
+                {
+                    if (palavra[i] == letraTentadas[j])
+                    {
+                        palavra_escondida += palavra[i];
+                        letraAcertada = true;
+                    }
+                }
+            }
+
+            if (!letraAcertada)
+            {
+                palavra_escondida += "_";
+            }
+        }
+        return palavra_escondida;
+    }
+}
